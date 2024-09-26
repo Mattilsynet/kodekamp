13368fead8547fb68d7dc6757feac6b20e156090
Kristoffer Høegh Mysen <kristoffer.hoegh.mysen@mattilsynet.no>
2024-09-26 08:54:38 +0200

    Lagt til endepunkt for å motta json som text

--- kotlin/quarkus-kotlin-rest/src/main/kotlin/no/mattilsynet/kodekamp/GameResource.kt
+++ kotlin/quarkus-kotlin-rest/src/main/kotlin/no/mattilsynet/kodekamp/GameResource.kt
@@ -1,16 +1,22 @@
 package no.mattilsynet.kodekamp
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.POST
 import jakarta.ws.rs.Path
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
 import jakarta.ws.rs.core.Response
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 @Path("/game")
 class GameResource(
-    private val gameEngine: GameEngine
+    private val gameEngine: GameEngine,
+    private val objectMapper: ObjectMapper
 ) {
+
+    private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
@@ -23,4 +29,14 @@ class GameResource(
     fun input(input: Input): Response {
         return Response.ok(gameEngine.process(input)).build()
     }
+
+    @POST
+    @Path("/text")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun inputText(inputString: String): Response {
+        logger.info("Received input: $inputString")
+        val input: Input = objectMapper.readValue(inputString, Input::class.java)
+        return Response.ok(input).build()
+    }
+
 }
